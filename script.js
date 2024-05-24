c99e075fe3f08b0d0793057bfc5219170224d33a
Dario Enrique Ochoa Posse <darioe.upwork@gmail.com>
2024-05-24 18:03:51 -0300

    polishing even more

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// Genera puntos (estrellas) de forma aleatoria y los agrega al cuerpo de la página
+function generateStars() {
+  const body = document.querySelector("body");
+  const numStars = window.innerWidth > 768 ? 100 : 50; // Número de estrellas: más en pantallas grandes, menos en pequeñas
+
+  for (let i = 0; i < numStars; i++) {
+    const star = document.createElement("div");
+    star.classList.add("star");
+    star.style.top = `${Math.random() * 100}%`;
+    star.style.left = `${Math.random() * 100}%`;
+    body.appendChild(star);
+  }
+}
+
+// Llama a la función para generar estrellas cuando se carga la página
+window.onload = generateStars;
+
+// Regenerar estrellas en cambio de tamaño de la ventana
+window.onresize = () => {
+  document.querySelectorAll(".star").forEach((star) => star.remove());
+  generateStars();
+};
+
 $(document).ready(function () {
   // Inicializa el primer carrusel
   $(".video-carousel-1").slick({
@@ -34,57 +57,4 @@ $(document).ready(function () {
   $(".next-button-2").click(function () {
     $(".video-carousel-2").slick("slickNext");
   });
-
-  // Inicializa el tercer carrusel
-  $(".video-carousel-3").slick({
-    infinite: false,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    dots: false,
-  });
-
-  // Botones de navegación para el tercer carrusel
-  $(".prev-button-3").click(function () {
-    $(".video-carousel-3").slick("slickPrev");
-  });
-
-  $(".next-button-3").click(function () {
-    $(".video-carousel-3").slick("slickNext");
-  });
 });
-
-// Inicializa el tercer carrusel
-$(".video-carousel-3").slick({
-  infinite: false,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  dots: false,
-});
-
-// Botones de navegación para el tercer carrusel
-$(".prev-button-3").click(function () {
-  $(".video-carousel-3").slick("slickPrev");
-});
-
-$(".next-button-3").click(function () {
-  $(".video-carousel-3").slick("slickNext");
-});
-
-// Genera puntos (estrellas) de forma aleatoria y los agrega al cuerpo de la página
-function generateStars() {
-  const numStars = 100; // Número de estrellas
-  const body = document.querySelector("body");
-
-  for (let i = 0; i < numStars; i++) {
-    const star = document.createElement("div");
-    star.classList.add("star");
-    star.style.top = `${Math.random() * 100}%`;
-    star.style.left = `${Math.random() * 100}%`;
-    body.appendChild(star);
-  }
-}
-
-// Llama a la función para generar estrellas cuando se carga la página
-window.onload = generateStars;
